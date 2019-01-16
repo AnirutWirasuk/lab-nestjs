31ef9fd0ada81966cadd07cb1e0ee8bdd26e86db
Anirut Wirasuk <a.w.wirasuk@hotmail.com>
2019-01-16 14:53:33 +0700

    cat controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		GraphQLModule.forRoot({
+			typePaths: ['./**/*.graphql']
+		}),
+		MongooseModule.forRoot('mongodb://localhost/nest'),
+		CatsModule],
+  controllers: [AppController, CatsController],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
